82b0fefb5ca77649950105f09b67eee3dc20e543
Isidora Vujasin <isidoravujasin@Isidoras-MacBook-Air.local>
2026-01-20 15:59:13 +0100

    feat: replace Nuxt welcome with basic app layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main class="app">
+    <h1>Nuxt color picker</h1>
+    <p>Work in progress</p>
+  </main>
 </template>
+
+<style>
+  .app {
+    padding: 2rem;
+    font-family: system-ui, sans-serif;
+  }
+</style>
